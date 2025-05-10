9a226117aba30ac70a55d403e3c411f6ae7db3c9
darkarc <2350230863@qq.com>
2025-05-10 11:22:51 +0800

    修改图片

--- data/team.js
+++ data/team.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: darkarc 2350230863@qq.com
+ * @Date: 2025-05-08 16:02:24
+ * @LastEditors: darkarc 2350230863@qq.com
+ * @LastEditTime: 2025-05-10 10:40:10
+ * @FilePath: \网页\data\team.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 // 团队成员数据
 const teamData = {
     // 作者信息
@@ -5,36 +13,6 @@ const teamData = {
         {
             college: "自动化学院",
             grade: "博一/控制科学与工程",
-            studentId: "",
-            phone: ""
-        },
-        {
-            college: "自动化学院",
-            grade: "研一/控制科学与工程",
-            studentId: "",
-            phone: ""
-        },
-        {
-            college: "自动化学院",
-            grade: "研一/控制工程",
-            studentId: "",
-            phone: ""
-        },
-        {
-            college: "自动化学院",
-            grade: "研一/控制工程",
-            studentId: "",
-            phone: ""
-        },
-        {
-            college: "自动化学院",
-            grade: "大三/",
-            studentId: "",
-            phone: ""
-        },
-        {
-            college: "自动化学院",
-            grade: "研一/控制科学与工程",
             studentId: "",
             phone: ""
         }
@@ -47,10 +25,6 @@ const teamData = {
             title: "教授",
             phone: ""
         },
-        {
-            unit: "南京航空航天大学自动化学院",
-            title: "副研究员",
-            phone: ""
-        }
+
     ]
 };
